672b63b29d3415b380beaf8e8c17134900d60c93
Jenny Gallegos <dev.jenny.ga@gmail.com>
2025-07-08 09:31:08 -0600

    Update the data fetching

--- js/data.js
+++ js/data.js
@@ -1,6 +1,6 @@
 // This file reads the data from the items.json file and converts it into a JavaScript object
 
-const itemsData = fetch('data/items.json')
+const itemsData = fetch('https://raw.githubusercontent.com/LeagueDamageCalculator/LeagueDamageCalculator/main/data/items.json')
   .then(response => response.json())
   .then(data => {
     // Convert the data to a JavaScript object
